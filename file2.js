0b5ef8885d0dbb984e7f7e26f0543d7b46bd9af0
Hemshaanth P <hemshaanth@gmail.com>
2024-12-03 20:14:40 +0530

    file 2 modify

--- file2.js
+++ file2.js
@@ -2,3 +2,4 @@ console.log("Hello, world!");
 console.log(new Date().toLocaleTimeString());
 console.log("by Hemshaanth P (API/Apigee Developer, CATERPILLAR");
 console.log("India");
+console.log("hems");
